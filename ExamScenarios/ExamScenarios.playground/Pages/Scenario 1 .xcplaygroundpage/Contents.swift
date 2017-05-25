ef44a77ef8da0351278d607bf19b834656265388
Ben Laurin <benlaurin@Ben-Laurin.local>
2017-05-25 12:24:11 -0400

    finished the orange square thing

--- ExamScenarios/ExamScenarios.playground/Pages/Scenario 1 .xcplaygroundpage/Contents.swift
+++ ExamScenarios/ExamScenarios.playground/Pages/Scenario 1 .xcplaygroundpage/Contents.swift
@@ -25,6 +25,47 @@ let canvas = Canvas(width: 400, height: 600)
 canvas.fillColor = Color(hue: 20, saturation: 89, brightness: 94, alpha: 100)
 canvas.drawRectangle(centreX: 200, centreY: 300, width: 400, height: 600)
 
+//background
+canvas.fillColor = Color(hue: 20, saturation: 78, brightness: 98, alpha: 100)
+canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 400, height: 600)
+
+canvas.translate(byX: 325, byY: 3)
+canvas.rotate(by: 45)
+
+//square grid
+canvas.fillColor = Color(hue: 20, saturation: 78, brightness: 50, alpha: 100)
+canvas.drawShapesWithBorders = false
+
+for x in stride(from: 0, to: 400, by: 100){
+    for y in stride(from: 0, to: 400, by: 100){
+        
+        //dark brown
+        if x + y == 100 || x + y == 500 {
+            canvas.fillColor = Color(hue: 20, saturation: 82, brightness: 31, alpha: 100)
+        }
+        //light brown
+        if x + y == 200 || x + y == 400 {
+            canvas.fillColor = Color(hue: 20, saturation: 82, brightness: 56, alpha: 100)
+        }
+        // very light brown
+        if x + y == 300 || x + y == 300 {
+            canvas.fillColor = Color(hue: 20, saturation: 82, brightness: 77, alpha: 100)
+        }
+        
+        //draw black
+        if x + y == 0 || x + y == 600 {
+            canvas.fillColor = Color(hue: 20, saturation: 82, brightness: 0, alpha: 100)
+        }
+        
+        
+        
+        
+        
+        
+        canvas.drawRectangle(bottomLeftX: x, bottomLeftY: y, width: 100, height: 100)
+    }
+}
+
 
 
 //: ## Template code
